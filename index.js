35652551b3a72dfb866847dd301600017e7425cd
trinh hong thuc <trinhhongthuc57@gmail.com>
2020-06-28 14:56:41 +0700

    done

--- index.js
+++ index.js
@@ -20,33 +20,19 @@ deleteAc.addEventListener( "click", () => {
 
 result.addEventListener( "click", () => {
     let string = input.value;
-    let value = 0;
-    let value2 = 0;
   
-    if( string.indexOf( "*" ) !== -1 ) {
-        let resultNumberString = string.indexOf( "*" );
-        value = string.slice( 0, resultNumberString);
-        value2 = string.slice ( resultNumberString +1);
-        display.innerHTML =  parseInt(value) * parseInt(value2) ;
-        input.value = '';
-    } else if (string.indexOf( "+" ) !== -1 )  {
-        let resultNumberString = string.indexOf( "+" );
-        value = string.slice( 0, resultNumberString);
-        value2 = string.slice ( resultNumberString +1);
-        display.innerHTML =  parseInt(value) + parseInt(value2) ;
-        input.value = '';
-    } else if (string.indexOf( "-" ) !== -1) {
-        let resultNumberString = string.indexOf( "-" );
-        value = string.slice( 0, resultNumberString);
-        value2 = string.slice ( resultNumberString +1);
-        display.innerHTML =  parseInt(value) - parseInt(value2) ;
-        input.value = '';
-    } else if (string.indexOf( "/" ) !== -1){
-        let resultNumberString = string.indexOf( "/" );
-        value = string.slice( 0, resultNumberString);
-        value2 = string.slice ( resultNumberString +1);
-        display.innerHTML =  parseInt(value) / parseInt(value2) ;
-        input.value = '';
+  
+    if( string.lastIndexOf( "*" ) !== -1 ) {
+        nhan();
+    } else if (string.lastIndexOf( "+" ) !== -1 )  {
+        cong();
+    } else if (string.lastIndexOf( "-" ) !== -1) {
+       tru();
+    } else if (string.lastIndexOf( "/" ) !== -1){
+        chia();
+    } else {
+        display.innerHTML =  input.value ;
+        input.value = "";
     }
 })
 
@@ -55,3 +41,47 @@ deleteElement.addEventListener('click', () => {
     let resultValue = input.value;
     input.value = resultValue.slice(0, resultValue.length -1);
 })
+
+ let cong = () => {
+    let string = input.value;
+    let value = 0;
+    let value2 = 0;
+    let resultNumberString = string.lastIndexOf( "+" );
+    value = string.slice( 0, resultNumberString);
+    value2 = string.slice ( resultNumberString +1);
+    display.innerHTML =  parseInt(value) + parseInt(value2) ;
+    input.value = parseInt(value) + parseInt(value2);
+ }
+
+ let tru = () => {
+    let string = input.value;
+    let value = 0;
+    let value2 = 0;
+    let resultNumberString = string.lastIndexOf( "-" );
+    value = string.slice( 0, resultNumberString);
+    value2 = string.slice ( resultNumberString +1);
+    display.innerHTML =  parseInt(value) - parseInt(value2) ;
+    input.value = parseInt(value) - parseInt(value2);
+ }
+
+ let nhan = () => {
+    let string = input.value;
+    let value = 0;
+    let value2 = 0;
+    let resultNumberString = string.lastIndexOf( "*" );
+    value = string.slice( 0, resultNumberString);
+    value2 = string.slice ( resultNumberString +1);
+    display.innerHTML =  parseInt(value) * parseInt(value2) ;
+    input.value = parseInt(value) * parseInt(value2);
+ }
+
+ let chia = () => {
+    let string = input.value;
+    let value = 0;
+    let value2 = 0;
+    let resultNumberString = string.lastIndexOf( "/" );
+    value = string.slice( 0, resultNumberString);
+    value2 = string.slice ( resultNumberString +1);
+    display.innerHTML =  parseInt(value) / parseInt(value2) ;
+    input.value = parseInt(value) / parseInt(value2);
+ }
